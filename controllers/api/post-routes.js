4b97ec77f5065088f1ca312dec5beeb71206f8a6
Precious <preciouseneagu8@icloud.com>
2022-10-12 11:29:45 -0700

    huh

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -7,26 +7,7 @@ const {
 const withAuth = require('../../utils/auth');
 
 
-// Get all posts
-router.get("/", (req, res) => {
-    Post.findAll({
-            attributes: ["id", "content", "title", "created_at"],
-            order: [
-                ["created_at", "DESC"]
-            ],
-            include: [{
-                    model: User,
-                    attributes: ["username"],
-                },
-                {
-                    model: Comment,
-                    attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
-                    include: {
-                        model: User,
-                        attributes: ["username"],
-                    },
-                },
-            ],
+
         })
         .then((dbPostData) => res.json(dbPostData))
         .catch((err) => {
